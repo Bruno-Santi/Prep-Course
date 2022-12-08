319875827ecc0661a977a10fdb6bb2dca882e8a2
Bruno Santimaria <brun0.23@outlook.com>
2022-12-08 15:29:10 -0300

    Resueltos algunos ejercicios del homework JS III

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -3,18 +3,24 @@
 function devolverPrimerElemento(array) {
   // Devuelve el primer elemento de un  array (pasado por parametro)
   // Tu código:
+  return array[0];
 }
 
 
 function devolverUltimoElemento(array) {
   // Devuelve el último elemento de un array
   // Tu código:
+  var ultimoElemento = array[array.length - 1];
+  return ultimoElemento;
+
 }
 
 
 function obtenerLargoDelArray(array) {
   // Devuelve el largo de un array
   // Tu código:
+  var lengthOfArray = array.length;
+  return lengthOfArray;
 }
 
 
@@ -23,6 +29,20 @@ function incrementarPorUno(array) {
   // Aumenta cada entero por 1
   // y devuelve el array
   // Tu código:
+  
+      var i = 0;
+      var newArray = [];
+      
+      do {
+       array[i] = array[i] + 1;
+       newArray.push(array[i]);
+       i++;
+    
+      } while (i < array.length) {
+       return newArray;
+      }
+    }
+  
 }
 
 
@@ -30,6 +50,8 @@ function agregarItemAlFinalDelArray(array, elemento) {
   // Añade el "elemento" al final del array
   // y devuelve el array
   // Tu código:
+  array.push(elemento);
+  return array;
 }
 
 
@@ -38,6 +60,8 @@ function agregarItemAlComienzoDelArray(array, elemento) {
   // y devuelve el array
   // Pista: usa el método `.unshift`
   // Tu código:
+  array.unshift(elemento);
+  return array;
 }
 
 
@@ -47,6 +71,8 @@ function dePalabrasAFrase(palabras) {
   // con espacios entre cada palabra
   // Ejemplo: ['Hello', 'world!'] -> 'Hello world!'
   // Tu código:
+  var nuevoStr = palabras.join(' ')
+  return nuevoStr;
 }
 
 
@@ -54,6 +80,14 @@ function arrayContiene(array, elemento) {
   // Comprueba si el elemento existe dentro de "array"
   // Devuelve "true" si está, o "false" si no está
   // Tu código:
+  for(var i = 0; i < array.length; i++){
+    if(array[i] === elemento) {
+       return true;
+     } 
+       
+     
+   }
+   return false;
 }
 
 
